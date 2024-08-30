7022ec1f3795fa1b5467eec059784025b4bf8407
toshkatta <toshkatta@gmail.com>
2024-08-31 00:20:18 +0300

    graph improvements

--- app/views/pages/CoinDetails/components/CandleGraph/index.jsx
+++ app/views/pages/CoinDetails/components/CandleGraph/index.jsx
@@ -28,8 +28,10 @@ ChartJS.register(
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 );
+
+ChartJS.defaults.font.family = "'Roboto', Helvetica, Arial, sans-serif";
 
 const candleToGraphCandle = (c) => ({
   x: c.date,
@@ -37,6 +39,52 @@ const candleToGraphCandle = (c) => ({
   c: c.close,
   s: [c.open, c.close],
 });
+
+const hoverLine = {
+  id: 'hoverLine',
+  defaults: {
+      width: 1,
+      color: '#FF4949',
+      dash: [3, 3],
+  },
+  afterInit: (chart) => {
+    chart.hoverLine = {
+      x: 0,
+      y: 0,
+    };
+  },
+  afterEvent: (chart, args) => {
+    const { inChartArea } = args;
+    const { x, y } = args.event;
+
+    chart.hoverLine = { x, y, draw: inChartArea };
+    chart.draw();
+  },
+  beforeDatasetsDraw(chart) {
+    const {
+      ctx,
+      chartArea: {
+        top,
+        bottom,
+      },
+    } = chart;
+
+    const { x, draw } = chart.hoverLine;
+    if (!draw) return;
+
+    ctx.save();
+
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'rgb(0 0 0 / 0.5)';
+    ctx.setLineDash([3, 9]);
+    ctx.moveTo(x, bottom);
+    ctx.lineTo(x, top);
+    ctx.stroke();
+
+    ctx.restore();
+  },
+};
 
 const CandleGraph = () => {
   const candles  = useSelector(selectCandles);
@@ -63,6 +111,23 @@ const CandleGraph = () => {
         display: false,
       },
       tooltip: {
+        displayColors: false,
+        cornerRadius: 32,
+        titleAlign: 'center',
+        titleFont: {
+          size: 10,
+        },
+        bodyFont: {
+          size: 16,
+          weight: 'bold',
+        },
+        padding: {
+          left: 20,
+          right: 20,
+          top: 12,
+          bottom: 12,
+        },
+        backgroundColor: 'rgb(0 0 0 / 0.9)',
         callbacks: {
           label: (context) => new Intl.NumberFormat(
             locale, {
@@ -72,11 +137,38 @@ const CandleGraph = () => {
           ).format(context.parsed.y),
         },
       },
+      hoverLine: {
+        color: 'black',
+      },
     },
     scales: {
       y: {
         min: minPrice * 0.995,
         max: maxPrice * 1.005,
+        grid: {
+          display: false,
+        },
+      },
+      x: {
+        offsetAfterAutoskip: true,
+        ticks: {
+          autoSkip: true,
+          autoSkipPadding: 28,
+          maxRotation: 0,
+          padding: 8,
+          font: {
+            size: 14,
+            weight: 'bold',
+          },
+        },
+        border: {
+          dash: [8, 8],
+        },
+        grid: {
+          tickBorderDash: [8, 8],
+          tickBorderDashOffset: 24,
+          lineWidth: 2,
+        },
       },
     },
     interaction: {
@@ -107,12 +199,14 @@ const CandleGraph = () => {
           yAxisKey: 's',
         },
         backgroundColor: 'rgb(42 63 252)',
+        borderRadius: Number.MAX_VALUE,
+        borderSkipped: false,
       },
     ],
   };
 
   return (
-    <Bar options={options} data={data} />
+    <Bar options={options} data={data} plugins={[hoverLine]} />
   );
 };
 
